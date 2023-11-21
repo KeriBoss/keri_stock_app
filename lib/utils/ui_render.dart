0375ee415064cf9e1fcbc37d428ed603f4a1768f
KeriBoss <115531515+KeriBoss@users.noreply.github.com>
2023-11-21 15:59:03 +0700

    finish push message android

--- lib/utils/ui_render.dart
+++ lib/utils/ui_render.dart
@@ -4,8 +4,6 @@ import 'dart:typed_data';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:stock_market_project/core/extension/number_extension.dart';
 
 class UiRender {
   const UiRender._();
@@ -52,7 +50,7 @@ class UiRender {
     TextAlign textAlign = TextAlign.center,
     String confirmText = 'OK',
   }) async {
-    bool? result = await showCupertinoDialog(
+    bool result = await showCupertinoDialog(
       barrierDismissible: true,
       context: context,
       builder: (BuildContext ctx) {
@@ -91,7 +89,7 @@ class UiRender {
       },
     );
 
-    return result ?? false;
+    return false;
   }
 
   static Future<void> showDialog(
@@ -169,7 +167,7 @@ class UiRender {
     required Widget child,
     String confirmText = 'Xác nhận',
   }) async {
-    bool? result = await showCupertinoDialog(
+    bool result = await showCupertinoDialog(
       barrierDismissible: true,
       context: context,
       builder: (BuildContext ctx) {
@@ -203,65 +201,6 @@ class UiRender {
       },
     );
 
-    return result ?? false;
-  }
-
-  static Future<String> showSingleTextFieldDialog(
-    BuildContext context,
-    TextEditingController? controller, {
-    String? title,
-    String? hintText,
-  }) async {
-    return await showPlatformDialog(
-      barrierDismissible: true,
-      context: context,
-      builder: (BuildContext ctx) {
-        return PlatformAlertDialog(
-          title: Text(title ?? ''),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextFormField(
-                textAlign: TextAlign.center,
-                controller: controller,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: hintText ?? '',
-                  hintStyle: TextStyle(
-                    fontFamily: 'Trebuchet MS',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15.size,
-                    color: const Color(0xff8D8D8C),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            // The "Yes" button
-            CupertinoDialogAction(
-              onPressed: () {
-                context.router.pop(controller?.text ?? '');
-              },
-              isDefaultAction: true,
-              child: Text(
-                'Gửi',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-            ),
-            // The "No" button
-            CupertinoDialogAction(
-              onPressed: () {
-                context.router.pop('');
-              },
-              isDestructiveAction: true,
-              child: const Text('Huỷ'),
-            )
-          ],
-        );
-      },
-    );
+    return false;
   }
 }
